5822a52de859db01759a9120dc4c4da1fa4e4d73
Ryzn <rahulblue17@gmail.com>
2024-08-31 16:18:02 +0530

    Add Navbar and Footer components to layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 
 export default function Home() {
   return (
-  <h1 className="text-bold text-teal-500">HELLO</h1>
+  <h1 className="text-bold text-teal-500 text-2xl">
+    HELLO
+    </h1>
   );
 }
